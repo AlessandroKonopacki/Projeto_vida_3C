d4eb33d46411c2d2d598c1a7a8766e785611baa5
AlessandroKonopacki <146283173+AlessandroKonopacki@users.noreply.github.com>
2026-05-06 16:27:23 -0300

    q

--- main.js
+++ main.js
@@ -3,14 +3,11 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
-
-    // remove de todos
     for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
       textos[j].classList.remove("ativo");
     }
 
-    // ativa o clicado
     botoes[i].classList.add("ativo");
     textos[i].classList.add("ativo");
   };
