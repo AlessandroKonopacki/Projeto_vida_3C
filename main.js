fb0fe9e36a7c6af76272988ed3b616d5eedc5f0f
AlessandroKonopacki <146283173+AlessandroKonopacki@users.noreply.github.com>
2026-05-06 16:22:06 -0300

    a

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ for (let i = 0; i < botoes.length; i++) {
     botoes[i].classList.add("ativo");
     textos[i].classList.add("ativo");
     }
-    botoes[j].classList.remove("ativo");
-    textos[j].classList.remove("ativo");
+    botoes[i].classList.remove("ativo");
+    textos[i].classList.remove("ativo");
   };
 }
